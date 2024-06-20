8913f8900ce113007b0db19cf27c81bb661fad17
Andrew Brown <arbrown83@gmail.com>
2024-06-20 10:15:19 -0400

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'redsox': ['RedSox'],
+      'celtics': ['Celtics'],
+      'bruins': ['Trebuchet MS', 'sans-serif'],
+      'patriots': ['Patriots'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
